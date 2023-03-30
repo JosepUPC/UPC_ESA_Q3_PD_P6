ea4881722453bdca1cde9b96f100d52c817066e8
JosepUPC <github.jeb@gmail.com>
2023-03-30 19:34:41 +0200

    Master_Modifier

--- P6_1/src/main.cpp
+++ P6_1/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
 
-void setup() {
-  // put your setup code here, to run once:
+File myFile;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.print("Iniciando SD ...");
+  if (!SD.begin(5)) {
+    Serial.println("No se pudo inicializar");
+    return;
+  }
+  Serial.println("inicializacion exitosa");
+ 
+  myFile = SD.open("/archivo.txt");//abrimos  el archivo 
+  if (myFile) {
+    Serial.println("archivo.txt:");
+    while (myFile.available()) {
+    	Serial.write(myFile.read());
+    }
+    myFile.close(); //cerramos el archivo
+  } else {
+    Serial.println("Error al abrir el archivo");
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  
 }
